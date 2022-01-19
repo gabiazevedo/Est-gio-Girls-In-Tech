cf07c0be62be7ff4c4659c4c0ed4cc21a48be4f6
gabiazevedo <gabiazevedoms@gmail.com>
2022-01-19 20:20:13 -0300

    docs: Adiciona comentários com links de consulta

--- moduloZero/1015-distanceBetweenTwoPoints/distance.js
+++ moduloZero/1015-distanceBetweenTwoPoints/distance.js
@@ -7,3 +7,11 @@ let [x2, y2] = lines.shift().split(' ').map(n => Number(n));
 const distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)).toFixed(4);
 
 console.log(distance);
+
+// Links que guiaram o desenvolvimento
+
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/shift
+
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/sqrt
+
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/pow
