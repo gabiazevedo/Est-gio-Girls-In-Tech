043e775c577d5c05339adfbdf281ebfb5313f982
gabiazevedo <gabiazevedoms@gmail.com>
2022-01-19 17:58:35 -0300

    feat: Sobe exercício Snack - Plataforma beecrowd

---
+++ moduloZero/1038-snack/snack.js
@@ -0,0 +1,25 @@
+var input = require('fs').readFileSync('./moduloZero/1038-snack/stdin', 'utf8');
+var lines = input.split('\n');
+
+const [productCode, productQuantity] = lines.toString().replace(' ', '').split('').map(item => Number(item));;
+
+switch (productCode) {
+  case 1:
+    console.log(`Total: R$ ${(4.00 * productQuantity).toFixed(2)}`);
+    break;
+  case 2:
+    console.log(`Total: R$ ${(4.50 * productQuantity).toFixed(2)}`);
+    break;
+  case 3:
+    console.log(`Total: R$ ${(5.00 * productQuantity).toFixed(2)}`);
+    break;
+  case 4:
+    console.log(`Total: R$ ${(2.00 * productQuantity).toFixed(2)}`);
+    break;
+  case 5:
+    console.log(`Total: R$ ${(1.50 * productQuantity).toFixed(2)}`);
+    break;
+  default:
+    console.log(`Produto inválido! Digite um código válido.`)
+    break;
+}
