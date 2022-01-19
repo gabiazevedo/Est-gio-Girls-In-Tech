80146bde7a1b27df3293c46ff494ffcc7c942d6e
gabiazevedo <gabiazevedoms@gmail.com>
2022-01-19 14:22:18 -0300

    feat: Adiciona case 2 - Salário com Bônus

---
+++ moduloZero/1009-bonusSalary/index.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('./moduloZero/1009-bonusSalary/stdin', 'utf8');
+var lines = input.split('\n');
+
+const [name, salary, sales] = lines;
+
+const comission = Number(sales) * (15 / 100);
+const totalSalary = Number(salary) + comission;
+
+console.log(`TOTAL = R$ ${totalSalary.toFixed(2)}`);
+console.log(`O funcionário ${name} recebeu um TOTAL = R$ ${totalSalary.toFixed(2)} de salário + comissão.`)
