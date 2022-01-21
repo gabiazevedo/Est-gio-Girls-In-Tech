daa3d02ad3f9c4b794120a6feb66966cd3a8d99e
gabiazevedo <gabiazevedoms@gmail.com>
2022-01-21 16:28:16 -0300

    feat: Adiciona case 5 - Time conversion

---
+++ moduloZero/1019-timeConversion/timeConversion.js
@@ -0,0 +1,7 @@
+var input = require('fs').readFileSync('./moduloZero/1019-timeConversion/stdin', 'utf8');
+var lines = input.split('\n').toString();
+
+console.log(new Date(Number(lines) * 1000).toISOString().substring(11, 19));
+
+// O código funciona de acordo com a proposta do exercício na plataforma do beecrowd,
+// porém, quando executado lá, dá Runtime Error
