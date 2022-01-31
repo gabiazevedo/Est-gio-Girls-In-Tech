e830e7daecefa269fc739ee5a6deb81a60385031
gabiazevedo <gabiazevedoms@gmail.com>
2022-01-31 12:58:47 -0300

    feat: Sobe case Fibonacci Array e PUM

--- moduloZero/1176-fibonacciArray/fibonacciArray.js
+++ moduloZero/1176-fibonacciArray/fibonacciArray.js
@@ -1,7 +1,6 @@
 var input = require('fs').readFileSync('./moduloZero/1176-fibonacciArray/stdin', 'utf8');
 var lines = input.split('\n');
-
-let testes = Number(lines[0]);
+lines.shift();
 
 let fibo = [];
   fibo[0] = 0;
@@ -10,5 +9,10 @@ let fibo = [];
 for (let i = 2; i <= 60; i += 1) {
   fibo[i] = fibo[i - 2] + fibo[i - 1];
 }
-console.log(fibo)
+
+const N = lines;
+
+for (let i = 0; i < N.length; i += 1) {
+  console.log(`Fib(${N[i]}) = ${fibo[N[i]]}`)
+}
 
