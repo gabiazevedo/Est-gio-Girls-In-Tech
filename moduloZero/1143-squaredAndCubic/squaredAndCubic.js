56e8bd3aceda2fd333cb870ebd4193bc8bf6223d
gabiazevedo <gabiazevedoms@gmail.com>
2022-01-31 13:37:41 -0300

    feat: Sobe cases Squared and Cubic & Ascendind and Descending

---
+++ moduloZero/1143-squaredAndCubic/squaredAndCubic.js
@@ -0,0 +1,6 @@
+var input = require('fs').readFileSync('./moduloZero/1143-squaredAndCubic/stdin', 'utf8');
+var lines = input.split('\n').toString();
+
+for (let i = 1; i <= lines; i += 1) {
+  console.log(`${i} ${i * i} ${i * i * i}`)
+}
