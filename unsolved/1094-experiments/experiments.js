3a76be346bbd9777674dcd72f6f4914c844ecde0
gabiazevedo <gabiazevedoms@gmail.com>
2022-02-08 18:49:42 -0300

    feat: Sobe cases keanu & experiments, sobe código curso JS_POO

--- unsolved/1094-experiments/experiments.js
+++
@@ -1,6 +0,0 @@
-var input = require('fs').readFileSync('./unsolved/1094-experiments/stdin', 'utf8');
-var lines = input.split('\n');
-lines.shift()
-console.log(lines[0])
-
-// falta resolução
--- unsolved/3342-keanu/keanu.js
+++
@@ -1,4 +0,0 @@
-var input = require('fs').readFileSync('./moduloZero/3342-keanu/stdin', 'utf8');
-var lines = input.split('\n');
-
-// falta resolução
