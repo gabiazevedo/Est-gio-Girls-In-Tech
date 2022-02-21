1f00f42397fcd8544eb560e7e27d2ec911b24c8f
gabiazevedo <gabiazevedoms@gmail.com>
2022-02-21 17:43:51 -0300

    feat: Adiciona códigos da primeira aula do curso JS Fundamentos

---
+++ moduloUm/Cursos_Alura/JS_Fundamentos/type-string.js
@@ -0,0 +1,11 @@
+const textoUm = 'Olá mundo!';
+const textoDois = 'Olá mundo!';
+const senha = 'senhaSuperSegura456!';
+const stringDeNumeros = '34567';
+
+const citacao = 'Meu nome é...';
+const meuNome = 'Gabriela';
+
+// concatenação (+)
+
+console.log(citacao + meuNome);
