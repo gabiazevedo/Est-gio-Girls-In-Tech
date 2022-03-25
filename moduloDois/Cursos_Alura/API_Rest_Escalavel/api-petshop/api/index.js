1d56cf93cb0878523a0c0c3dbb4c49fa82b6e36a
gabiazevedo <gabiazevedoms@gmail.com>
2022-03-25 17:00:14 -0300

    feat: Setup inicial API Petshop

---
+++ moduloDois/Cursos_Alura/API_Rest_Escalavel/api-petshop/api/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const config = require('config');
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+
+app.listen(config.get('api.porta'), () => console.log(`Server listening on port ${config.get('api.porta')}`));
