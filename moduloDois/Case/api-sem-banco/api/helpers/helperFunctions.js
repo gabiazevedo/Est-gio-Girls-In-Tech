98eda61fc3fce5f4edd13bb8f827db9388b0b47b
Vincenzo G Neto <vincenzovgn@gmail.com>
2022-05-16 08:34:00 -0300

    melhoria de sugestão: os métodos estão bem feitos, o unico 'cuidado' é em relação as filias estar dentro das sedes.

--- moduloDois/Case/api-sem-banco/api/helpers/helperFunctions.js
+++ moduloDois/Case/api-sem-banco/api/helpers/helperFunctions.js
@@ -1,8 +1,11 @@
+/**
+ * Esse arquivo está ótimo, basicamente você está usando um 'pedacinho' de Repository Pattners
+ */
 module.exports = {
   getAllMerchants: (data) => data || [],
 
   getMerchantById: (id, data) => {
-    const merchant = data.find(item => item.merchant.id === id); // retorna o primeiro elemento encontrado
+    const merchant = data.find((item) => item.merchant.id === id); // retorna o primeiro elemento encontrado
     return merchant;
   },
 
@@ -11,81 +14,82 @@ module.exports = {
     return newData;
   },
 
-  updateMerchant: (id, newData, data) => { // posicionar os parâmetros por prioridade
-    const index = data.findIndex(item => item.merchant.id === id);
+  updateMerchant: (id, newData, data) => {
+    // posicionar os parâmetros por prioridade
+    const index = data.findIndex((item) => item.merchant.id === id);
     data[index] = newData;
     return data[index];
   },
-  
+
   updateMerchantStatus: (id, newStatus, data) => {
-    const index = data.find(item => item.merchant.id === id);
+    const index = data.find((item) => item.merchant.id === id);
     index.merchant.status = newStatus["status"];
     return index;
   },
 
   deleteMerchant: (id, data) => {
-    const index = data.find(item => item.merchant.id === id);
+    const index = data.find((item) => item.merchant.id === id);
     data.splice(index, 1);
     return index;
   },
-  
+
   getBranchs: (id, data) => {
-    const merchant = data.find(item => item.merchant.id === id);
+    const merchant = data.find((item) => item.merchant.id === id);
     const branchs = merchant.merchant.branchs;
     return branchs;
   },
 
   getBranchById: (id, branchId, data) => {
-    const merchant = data.find(item => item.merchant.id === id);
+    const merchant = data.find((item) => item.merchant.id === id);
     const branchs = merchant.merchant.branchs;
-    const branchById = branchs.find(item => item.branchId === branchId);
+    const branchById = branchs.find((item) => item.branchId === branchId);
     return branchById;
   },
 
   createBranch: (id, newBranch, data) => {
-    const merchant = data.find(item => item.merchant.id === id);
+    const merchant = data.find((item) => item.merchant.id === id);
     merchant.merchant.branchs.push(newBranch);
     return newBranch;
   },
 
   updateBranch: (id, branchId, branchInfo, data) => {
-    const merchant = data.find(item => item.merchant.id === id);
+    const merchant = data.find((item) => item.merchant.id === id);
     const branchs = merchant.merchant.branchs;
-    let branchIndex = branchs.findIndex(item => item.branchId === branchId);
+    let branchIndex = branchs.findIndex((item) => item.branchId === branchId);
     branchs[branchIndex] = branchInfo;
     return branchInfo;
   },
 
   updateBranchStatus: (id, branchId, newBranchStatus, data) => {
-    const merchantIndex = data.find(item => item.merchant.id === id);
+    const merchantIndex = data.find((item) => item.merchant.id === id);
     const branchs = merchantIndex.merchant.branchs;
-    const branch = branchs.find(item => item.branchId === branchId);
+    const branch = branchs.find((item) => item.branchId === branchId);
     branch.status = newBranchStatus["status"];
     return branch;
   },
 
   sendMessageToBranch: (id, branchId, message, data) => {
-    const merchantIndex = data.find(item => item.merchant.id === id);
+    const merchantIndex = data.find((item) => item.merchant.id === id);
     const branchs = merchantIndex.merchant.branchs;
-    const branch = branchs.find(item => item.branchId === branchId);
+    const branch = branchs.find((item) => item.branchId === branchId);
 
     const date = {
       message,
-      date: new Date().toISOString()
-    }
-    
+      date: new Date().toISOString(),
+    };
+
     branch.messages.push(date);
     return branch.messages;
   },
 
   deleteBranch: (id, branchId, data) => {
-    const merchantIndex = data.find(item => item.merchant.id === id);
+    const merchantIndex = data.find((item) => item.merchant.id === id);
     const branchs = merchantIndex.merchant.branchs;
-    const branch = branchs.find(item => item.branchId === branchId);
+    const branch = branchs.find((item) => item.branchId === branchId);
     branchs.splice(branch, 1);
     return branch;
-  }
-}
+  },
+};
 
 /* getBranchsById: (data, id) => {
   const merchant = data.filter(item => item.merchant.id === id);
--- moduloDois/Case/api-sem-banco/api/routes/merchantRoutes.js
+++ moduloDois/Case/api-sem-banco/api/routes/merchantRoutes.js
@@ -1,19 +1,22 @@
-const router = require('express').Router();
-const MerchantController = require('../controllers/MerchantController');
-
-router.post('/', MerchantController.createMerchant)
-      .get('/', MerchantController.getMerchants)
-      .put('/:id', MerchantController.updateMerchant)
-      .delete('/:id', MerchantController.deleteMerchant)
-      .put('/:id/updateStatus', MerchantController.updateMerchantStatus)
-
-router.get('/:id/branchs', MerchantController.getBranchs)
-      .get('/:id/branchs/:branchId', MerchantController.getBranchById)
-      .put('/:id/branchs/:branchId', MerchantController.updateBranch)
-      .post('/:id/branchs', MerchantController.createBranch)
-      .put('/:id/branchs/:branchId/updateStatus', MerchantController.updateBranchStatus)
-      .post('/:id/branchs/:branchId/message', MerchantController.sendMessageToBranch)
-      .delete('/:id/branchs/:branchId', MerchantController.deleteBranch)
-      .get('/:id', MerchantController.getMerchantById) // primeira rota GET /:id deixar por último 
+const router = require("express").Router();
+const MerchantController = require("../controllers/MerchantController");
+/**
+ * Nas rotas estão sendo usadas um padrão (camelCase), isso é bom. Mas sugiro o uso sempre de quebac-case
+ * pois é comum no mercado esse uso, já que as rotas ficam melhor definidas com letras minúscula.
+ */
+router
+  .post("/", MerchantController.createMerchant)
+  .get("/", MerchantController.getMerchants)
+  .put("/:id", MerchantController.updateMerchant)
+  .delete("/:id", MerchantController.deleteMerchant)
+  .put("/:id/update-status", MerchantController.updateMerchantStatus)
+  .get("/:id/branchs", MerchantController.getBranchs)
+  .get("/:id/branchs/:branchId", MerchantController.getBranchById)
+  .put("/:id/branchs/:branchId", MerchantController.updateBranch)
+  .post("/:id/branchs", MerchantController.createBranch)
+  .put("/:id/branchs/:branchId/update-status", MerchantController.updateBranchStatus)
+  .post("/:id/branchs/:branchId/message", MerchantController.sendMessageToBranch)
+  .delete("/:id/branchs/:branchId", MerchantController.deleteBranch)
+  .get("/:id", MerchantController.getMerchantById); // primeira rota GET /:id deixar por último
 
 module.exports = router;
